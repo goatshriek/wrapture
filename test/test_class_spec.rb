81d971fb413624b359b61cd627fa2343458a9e23
Joel Anderson <joelanderson333@gmail.com>
2019-05-10 20:33:07 -0400

    add test for wrapper generation

--- test/test_class_spec.rb
+++ test/test_class_spec.rb
@@ -14,4 +14,30 @@ class ClassSpecTest < Minitest::Test
 
     assert normalized_spec != nil
   end
+
+  def test_generate_wrappers
+    test_spec = {
+      'name' => "TestClass",
+      'equivalent-struct' => {
+        'name' => 'test_struct',
+        'includes' => ['folder/include_file_1.h']
+      },
+      'functions' => [{
+        'name' => 'TestFunction1',
+        'params' => [{'name'=>'app_name','type'=>'const char *'}],
+        'wrapped-function' => {
+          'name' => 'test_native_function',
+          'includes' => ['folder/include_file_2.h'],
+          'params' => [
+            {'name' => 'equivalent-struct-pointer'},
+            {'name' => 'app_name'}
+          ]
+        }
+      }]
+    }
+
+    spec = Wrapture::ClassSpec.new test_spec
+
+    spec.generate_wrappers
+  end
 end
