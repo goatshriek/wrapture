aeade5e8df9b5ec32322d405c89cbcef4580088c
Joel Anderson <joelanderson333@gmail.com>
2019-05-18 09:54:15 -0400

    fix rubocop complaints in top level file

--- lib/wrapture.rb
+++ lib/wrapture.rb
@@ -1,6 +1,8 @@
-module Wrapture
+# frozen_string_literal: true
 
+##
+# Classes and functions for generating language wrappers
+module Wrapture
   require 'wrapture/class_spec'
   require 'wrapture/version'
-
 end
