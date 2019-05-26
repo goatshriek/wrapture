27f64dd91ccc41ed867515656a7da4dcbd201c02
Joel Anderson <joelanderson333@gmail.com>
2019-05-25 20:32:20 -0400

    move rest of existing tests into fixtures

--- test/test_class_spec.rb
+++ test/test_class_spec.rb
@@ -15,27 +15,7 @@ class ClassSpecTest < Minitest::Test
   end
 
   def test_generate_wrappers
-    class_name = 'TestClass'
-
-    test_spec = {
-      'name' => class_name,
-      'equivalent-struct' => {
-        'name' => 'test_struct',
-        'includes' => ['folder/include_file_1.h']
-      },
-      'functions' => [{
-        'name' => 'TestFunction1',
-        'params' => [{ 'name' => 'app_name', 'type' => 'const char *' }],
-        'wrapped-function' => {
-          'name' => 'test_native_function',
-          'includes' => ['folder/include_file_2.h'],
-          'params' => [
-            { 'name' => 'equivalent-struct-pointer' },
-            { 'name' => 'app_name' }
-          ]
-        }
-      }]
-    }
+    test_spec = load_fixture('basic_class')
 
     spec = Wrapture::ClassSpec.new test_spec
 
@@ -44,8 +24,8 @@ class ClassSpecTest < Minitest::Test
     refute_nil classes
     refute_empty classes
     assert classes.length == 2
-    assert classes.include? "#{class_name}.cpp"
-    assert classes.include? "#{class_name}.hpp"
+    assert classes.include? "#{test_spec['name']}.cpp"
+    assert classes.include? "#{test_spec['name']}.hpp"
 
     File.delete(*classes)
   end
