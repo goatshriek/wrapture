a3ecb40d7eb88003bd024c93aca1b2c7a39da030
Joel Anderson <joelanderson333@gmail.com>
2019-05-27 18:22:24 -0400

    add test for version syntax

---
+++ test/test_version.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'helper'
+
+require 'minitest/autorun'
+require 'wrapture'
+
+class VersionTest < Minitest::Test
+  def test_version_syntax
+    assert_match /\d+\.\d+\.\d+/, Wrapture::VERSION
+  end
+end
