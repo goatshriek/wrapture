d4dcf6e409347ca28a5202d4288523bec5dc3574
Joel Anderson <joelanderson333@gmail.com>
2019-05-28 21:14:48 -0400

    add test to ensure gemspec and version match

--- test/test_version.rb
+++ test/test_version.rb
@@ -6,6 +6,12 @@ require 'minitest/autorun'
 require 'wrapture'
 
 class VersionTest < Minitest::Test
+  def test_gemspec_version
+    spec = Gem::Specification.load('wrapture.gemspec')
+    spec_version = spec.version.to_s
+    assert_equal Wrapture::VERSION, spec_version
+  end
+
   def test_version_syntax
     assert_match(/\d+\.\d+\.\d+/, Wrapture::VERSION)
   end
