82dbce01bc3d4e3b2cd1e4f3e5be5420c2c3ee52
Joel Anderson <joelanderson333@gmail.com>
2019-07-07 12:14:10 -0400

    add basic class include implementation

--- test/helper.rb
+++ test/helper.rb
@@ -22,4 +22,28 @@ def validate_wrapper_results(spec, file_list)
   assert file_list.length == 2
   assert file_list.include? "#{spec['name']}.cpp"
   assert file_list.include? "#{spec['name']}.hpp"
+
+  class_includes = spec['includes'] || []
+
+  header_includes = []
+  File.open("#{spec['name']}.hpp").each do |line|
+    if(m=line.match(/#\s*include\s*["<](.*)[">]/))
+      header_includes << m[1]
+    end
+  end
+
+  class_includes.each do |class_include|
+    assert_includes(header_includes, class_include)
+  end
+
+  source_includes = []
+  File.open("#{spec['name']}.hpp").each do |line|
+    if(m=line.match(/#\s*include\s*["<](.*)[">]/))
+      source_includes << m[1]
+    end
+  end
+
+  class_includes.each do |class_include|
+    assert_includes(source_includes, class_include)
+  end
 end
