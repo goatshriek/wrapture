01d3e7efbf2e35be32f3aecf4204fe7e1a99ed9a
Joel Anderson <joelanderson333@gmail.com>
2019-07-07 14:51:45 -0400

    add test for indentation on header files

--- test/helper.rb
+++ test/helper.rb
@@ -28,13 +28,16 @@ def get_include_list(filename)
 end
 
 def validate_declaration_file(spec)
+  filename = "#{spec['name']}.hpp"
   class_includes = spec['includes'] || []
 
-  includes = get_include_list "#{spec['name']}.hpp"
+  includes = get_include_list filename
 
   class_includes.each do |class_include|
     assert_includes(includes, class_include)
   end
+
+  validate_indentation filename
 end
 
 def validate_definition_file(spec)
@@ -44,6 +47,33 @@ def validate_definition_file(spec)
 
   class_includes.each do |class_include|
     assert_includes(includes, class_include)
+  end
+end
+
+def validate_indentation(filename)
+  line_number = 0
+  indent_level = 0
+
+  File.open(filename).each do |line|
+    line_number += 1
+
+    next if line.strip.empty?
+
+    line.chomp!
+
+    indent_level -= 1 if line.end_with? '}', '};'
+
+    space_count = if line.end_with? ':'
+                    (indent_level - 1) * 2
+                  else
+                    indent_level * 2
+                  end
+
+    fail_msg = "#{filename}: line #{line_number} should have #{space_count}" \
+               ' spaces'
+    assert line.start_with?(' ' * space_count), fail_msg
+
+    indent_level += 1 if line.end_with? '{'
   end
 end
 
