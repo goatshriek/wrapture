0e919adf600ca6532a7bab4f1b0beb80bc3db792
Joel Anderson <joelanderson333@gmail.com>
2019-07-16 21:03:37 -0400

    update scope class to support spec hash

--- lib/wrapture/scope.rb
+++ lib/wrapture/scope.rb
@@ -4,11 +4,17 @@ module Wrapture
   # Describes a scope of one or more class specifications.
   class Scope
     # Creates an empty scope with no classes in it.
-    def initialize
+    def initialize(spec = nil)
       @classes = []
+
+      return if spec.nil? || !spec.key?('classes')
+
+      spec['classes'].each do |class_hash|
+        @classes << ClassSpec.new(class_hash)
+      end
     end
 
-    # Adds a spec to the scope
+    # Adds a spec to the scope.
     def add(spec)
       @classes.push(spec) if spec.is_a ClassSpec
     end
@@ -16,8 +22,10 @@ module Wrapture
     # Returns true if the scope contains the given type.
     def type?(type)
       @classes.each do |class_spec|
-        return true if class_spec.name == type
+        return class_spec if class_spec.name == type
       end
+
+      false
     end
   end
 end
