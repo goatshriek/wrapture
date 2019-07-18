f9196aef49616236c89cac2d53ce2e5bb4f673a6
Joel Anderson <joelanderson333@gmail.com>
2019-07-17 21:25:37 -0400

    add test for scope file generation

--- test/test_scope.rb
+++ test/test_scope.rb
@@ -13,5 +13,10 @@ class ScopeTest < Minitest::Test
     scope = Wrapture::Scope.new(test_spec)
 
     assert_equal(test_spec['classes'].count, scope.classes.count)
+
+    generated_files = scope.generate_wrappers
+    assert_equal(scope.classes.count, generated_files.count/2)
+
+    File.delete(*generated_files)
   end
 end
