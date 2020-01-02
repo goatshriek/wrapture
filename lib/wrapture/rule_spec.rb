e61b080891089175419597637510852764d4fc75
Joel Anderson <joelanderson333@gmail.com>
2020-01-01 20:57:31 -0500

    update documentation of RuleSpec::CONDITIONS

--- lib/wrapture/rule_spec.rb
+++ lib/wrapture/rule_spec.rb
@@ -69,8 +69,9 @@ module Wrapture
     #
     # The hash must have the following keys:
     # member-name:: the name of the struct member the rule applies to
-    # condition:: the condition this rule uses (supported values are held in the
-    # RuleSpec::CONDITIONS list)
+    # condition:: the condition this rule uses (supported values are the keys in
+    # the RuleSpec::CONDITIONS map, with the values mapped to the keys being the
+    # operator they translate to)
     # value:: the value to use in the condition check
     def initialize(spec)
       @spec = RuleSpec.normalize_spec_hash(spec)
