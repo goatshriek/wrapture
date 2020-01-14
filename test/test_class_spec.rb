cb6a3ef819bb65dcafab52ec3713dc0a4678b227
Joel Anderson <joelanderson333@gmail.com>
2020-01-13 19:29:47 -0500

    add failing test

--- test/test_class_spec.rb
+++ test/test_class_spec.rb
@@ -1,4 +1,20 @@
+# SPDX-License-Identifier: Apache-2.0
+
 # frozen_string_literal: true
+
+# Copyright 2019-2020 Joel E. Anderson
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 require 'helper'
 
@@ -10,6 +26,12 @@ class ClassSpecTest < Minitest::Test
   def test_invalid_type
     assert_raises(Wrapture::InvalidSpecKey) do
       Wrapture::ClassSpec.new(load_fixture('invalid_type_class'))
+    end
+  end
+
+  def test_no_name
+    assert_raises(Wrapture::MissingSpecKey) do
+      Wrapture::ClassSpec.new(load_fixture('no_name_class'))
     end
   end
 
