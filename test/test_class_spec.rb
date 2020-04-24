7917758a380fe386d9e213e5bdc3f64da9bf2db2
Joel <joelanderson333@gmail.com>
2020-04-24 12:13:20 -0400

    fix quote rubocop violations

--- test/test_class_spec.rb
+++ test/test_class_spec.rb
@@ -86,15 +86,15 @@ class ClassSpecTest < Minitest::Test
 
     member_regex = /^\s*#{class_name}\( int member/
     assert(file_contains_match(header_file, member_regex),
-           "the member constructor had a return type specified in the header")
+           'the member constructor had a return type specified in the header')
 
     spec_regex = /^\s*#{class_name}\( struct/
     assert(file_contains_match(header_file, spec_regex),
-           "the spec constructor had a return type specified in the header")
+           'the spec constructor had a return type specified in the header')
 
     destructor_regex = /^\s*~#{class_name}/
     assert(file_contains_match(header_file, destructor_regex),
-           "the destructor had a return type specified in the header")
+           'the destructor had a return type specified in the header')
 
     source_file = "#{class_name}.cpp"
     includes = get_include_list(source_file)
@@ -107,15 +107,15 @@ class ClassSpecTest < Minitest::Test
 
     member_regex = /^\s*#{class_name}::#{class_name}\( int member/
     assert(file_contains_match(source_file, member_regex),
-           "the member constructor had a return type specified when defined")
+           'the member constructor had a return type specified when defined')
 
     spec_regex = /^\s*#{class_name}::#{class_name}\( struct/
     assert(file_contains_match(source_file, spec_regex),
-           "the spec constructor had a return type specified when defined")
+           'the spec constructor had a return type specified when defined')
 
     destructor_regex = /^\s*#{class_name}::~#{class_name}/
     assert(file_contains_match(source_file, destructor_regex),
-           "the destructor had a return type specified when defined")
+           'the destructor had a return type specified when defined')
 
     wrapped_function = test_spec['constructors'][0]['wrapped-function']
     assert(file_contains_match(source_file, /= #{wrapped_function['name']}/))
