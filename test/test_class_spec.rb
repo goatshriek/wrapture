7acfe1fd23f7c5dfabbec1f4670c17e9879434a4
Joel Anderson <joelanderson333@gmail.com>
2020-04-24 10:36:22 -0400

    add failing tests for constructor and destructor types

--- test/test_class_spec.rb
+++ test/test_class_spec.rb
@@ -89,6 +89,19 @@ class ClassSpecTest < Minitest::Test
     forbidden = Wrapture::EQUIVALENT_STRUCT_KEYWORD
     assert(!file_contains_match(source_file, forbidden))
 
+    class_name = test_spec['name']
+    member_regex = /^\s*#{class_name}::#{class_name}\( int member/
+    assert(file_contains_match(source_file, member_regex),
+           "the member constructor had a return type specified")
+
+    spec_regex = /^\s*#{class_name}::#{class_name}\( struct/
+    assert(file_contains_match(source_file, spec_regex),
+           "the spec constructor had a return type specified")
+
+    destructor_regex = /^\s#{class_name}::~#{class_name}/
+    assert(file_contains_match(source_file, destructor_regex),
+           "the destructor had a return type specified")
+
     assert(file_contains_match(source_file, /= #{wrapped_function['name']}/))
 
     File.delete(*classes)
