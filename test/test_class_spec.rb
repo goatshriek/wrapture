0ee89ec1432b9fe2a118546124083f05eb322474
Joel <joelanderson333@gmail.com>
2020-04-24 14:35:33 -0400

    remove incorrect return types (#73)
    Return types were erroneously provided in the definitions of constructors
    and destructors of generated classes. Declarations were not affected,
    however. This patch corrects this behavior and adds tests to prevent
    regression.
    
    See issue #72 for more details on the bug.

--- test/test_class_spec.rb
+++ test/test_class_spec.rb
@@ -78,20 +78,49 @@ class ClassSpecTest < Minitest::Test
 
     spec = Wrapture::ClassSpec.new(test_spec)
 
-    classes = spec.generate_wrappers
-    validate_wrapper_results(test_spec, classes)
+    generated_files = spec.generate_wrappers
+    validate_wrapper_results(test_spec, generated_files)
 
-    source_file = "#{test_spec['name']}.cpp"
+    class_name = test_spec['name']
+    header_file = "#{class_name}.hpp"
+
+    member_regex = /^\s*#{class_name}\( int member/
+    assert(file_contains_match(header_file, member_regex),
+           'the member constructor had a return type specified in the header')
+
+    spec_regex = /^\s*#{class_name}\( struct/
+    assert(file_contains_match(header_file, spec_regex),
+           'the spec constructor had a return type specified in the header')
+
+    destructor_regex = /^\s*~#{class_name}/
+    assert(file_contains_match(header_file, destructor_regex),
+           'the destructor had a return type specified in the header')
+
+    source_file = "#{class_name}.cpp"
     includes = get_include_list(source_file)
-    wrapped_function = test_spec['constructors'][0]['wrapped-function']
-    assert_includes(includes, wrapped_function['includes'])
+    all_spec_includes(test_spec).each do |inc|
+      assert_includes(includes, inc)
+    end
 
     forbidden = Wrapture::EQUIVALENT_STRUCT_KEYWORD
-    assert(!file_contains_match(source_file, forbidden))
+    refute(file_contains_match(source_file, forbidden))
 
+    member_regex = /^\s*#{class_name}::#{class_name}\( int member/
+    assert(file_contains_match(source_file, member_regex),
+           'the member constructor had a return type specified when defined')
+
+    spec_regex = /^\s*#{class_name}::#{class_name}\( struct/
+    assert(file_contains_match(source_file, spec_regex),
+           'the spec constructor had a return type specified when defined')
+
+    destructor_regex = /^\s*#{class_name}::~#{class_name}/
+    assert(file_contains_match(source_file, destructor_regex),
+           'the destructor had a return type specified when defined')
+
+    wrapped_function = test_spec['constructors'][0]['wrapped-function']
     assert(file_contains_match(source_file, /= #{wrapped_function['name']}/))
 
-    File.delete(*classes)
+    File.delete(*generated_files)
   end
 
   def test_class_with_constant
