cc907b27374269c41ad781df90a3baba3fc3bb37
Joel Anderson <joelanderson333@gmail.com>
2020-05-09 10:50:48 -0400

    add failing test for keyword list

---
+++ test/test_constants.rb
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# frozen_string_literal: true
+
+# Copyright 2020 Joel E. Anderson
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'helper'
+
+require 'fixture'
+require 'minitest/autorun'
+require 'wrapture'
+
+class ConstantsTest < Minitest::Test
+  def test_keywords_array
+    Wrapture.constants.select { |sym| sym.end_with?("KEYWORD") }.each do |word|
+      assert_includes(Wrapture::KEYWORDS, Wrapture.const_get(word))
+    end
+  end
+end
