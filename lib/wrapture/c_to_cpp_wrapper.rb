f076735087ae23ddda5b67032c1792f8a449e5ca
Joel Anderson <joel@goatshriek.com>
2025-01-09 13:02:31 -0500

    remove unused code

--- lib/wrapture/c_to_cpp_wrapper.rb
+++ lib/wrapture/c_to_cpp_wrapper.rb
@@ -48,14 +48,6 @@ module Wrapture
       wrapper.source_files
     end
 
-    # Generates C++ source files, returning a list of the files generated. This
-    # is equivalent to instantiating a wrapper with the given spec, and then
-    # calling write_files on that.
-    # def self.write_spec_source_files(spec, **kwargs)
-    #   wrapper = new(spec)
-    #   wrapper.write_source_files(**kwargs)
-    # end
-
     # Creates a C++ wrapper for a given spec.
     def initialize(spec)
       @spec = spec
@@ -233,46 +225,6 @@ module Wrapture
 
       [filename]
     end
-
-    # Generates the C++ declaration file, returning the name of the file
-    # generated.
-    # +dir+ specifies the directory that the file should be written into. The
-    # default is the current working directory.
-    # def write_declaration_file(dir: Dir.pwd)
-    #   File.open(File.join(dir, declaration_filename), 'w') do |file|
-    #     declare { |line| file.puts(line) }
-    #   end
-
-    #   declaration_filename
-    # end
-
-    # Generates the C++ definition file, returning the name of the file
-    # generated.
-    # +dir+ specifies the directory that the file should be written into. The
-    # default is the current working directory.
-    # def write_definition_file(dir: Dir.pwd)
-    #   File.open(File.join(dir, definition_filename), 'w') do |file|
-    #     self.class.define_spec(@spec) { |line| file.puts(line) }
-    #   end
-
-    #   definition_filename
-    # end
-
-    # Generates C++ source files, returning a list of the files generated.
-    # +dir+ specifies the directory that the files should be written into. The
-    # default is the current working directory.
-    # def write_source_files(dir: Dir.pwd)
-    #   if @spec.is_a?(Scope)
-    #     @spec.flat_map do |spec|
-    #       self.class.write_spec_source_files(spec, dir: dir)
-    #     end
-    #   elsif forward_declared?
-    #     [write_declaration_file(dir: dir),
-    #      write_definition_file(dir: dir)]
-    #   else
-    #     [write_definition_file(dir: dir)]
-    #   end
-    # end
 
     private
 
