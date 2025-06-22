8d7e5da9e8c15a65ff914700b54890083ca147ae
Joel Anderson <joel@goatshriek.com>
2025-06-22 13:57:15 -0400

    manually implement load

--- test/fixture.rb
+++ test/fixture.rb
@@ -26,20 +26,28 @@ end
 
 # Creates a build hash for the fixture corresponding to +name+.
 def fixture_build_hash(name)
+  # drop this badk to safe_load_file after Ruby 2.7 is dropped
   if YAML.respond_to?('safe_load_file')
     YAML.safe_load_file(fixture_build_path(name), symbolize_names: true)
   else
-    YAML.load_file(fixture_build_path(name))
+    filename = fixture_yaml_path(name)
+    File.open(filename, 'r:bom|utf-8') do |f|
+      YAML.safe_load(f, filename: filename, symbolize_names: true)
+    end
   end
 end
 
 # Creates a hash for a Wrapture spec, finding and loading the YAML file
 # corresponding to +name+.
 def fixture_hash(name)
+  # drop this badk to safe_load_file after Ruby 2.7 is dropped
   if YAML.respond_to?('safe_load_file')
     YAML.safe_load_file(fixture_yaml_path(name), symbolize_names: true)
   else
-    YAML.load_file(fixture_yaml_path(name))
+    filename = fixture_yaml_path(name)
+    File.open(filename, 'r:bom|utf-8') do |f|
+      YAML.safe_load(f, filename: filename, symbolize_names: true)
+    end
   end
 end
 
